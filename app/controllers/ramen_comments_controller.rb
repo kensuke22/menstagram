cbf5e1540d6b5f0a73bda5d7f58511d15fddf961
kensuke22 <b430022222222@gmail.com>
2021-07-21 11:24:58 +0000

    [add] Googlemap機能

--- app/controllers/ramen_comments_controller.rb
+++ app/controllers/ramen_comments_controller.rb
@@ -5,14 +5,11 @@ class RamenCommentsController < ApplicationController
     comment = current_user.ramen_comments.new(ramen_comment_params)
     comment.ramen_image_id = @ramen_image.id
     comment.save
-    #redirect_to ramen_image_path(ramen_image)
   end
 
   def destroy
     @ramen_image = RamenImage.find(params[:ramen_image_id])
     RamenComment.find_by(id: params[:id], ramen_image_id: params[:ramen_image_id]).destroy
-    
-    #redirect_to ramen_image_path(params[:ramen_image_id])
   end
   
   private
